c1fc9d7086350124a9129d6b18fa158819fc35ec
Henrick <ricknogueira1231@gmail.com>
2025-04-30 20:51:36 -0300

    #D204 5 - Configurando Tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,9 @@
-export * from "./colors";
+export * from './colors'
+ 
+ export * from './space'
+ export * from './radii'
+ 
+ export * from './fonts'
+ export * from './font-sizes'
+ export * from './font-weigths'
+ export * from './line-heights'
